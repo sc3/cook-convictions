c79bb58c6c0fac0033da2eb6451a10dd4c5ac1d7
Brian Peterson <bepetersn@gmail.com>
2014-06-23 16:57:52 -0500

    Created cook-convictions from https://github.com/newsapps/tarbell-template

---
+++ tarbell_config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tarbell project configuration
+"""
+
+# Short project name
+NAME = "cook-convictions"
+
+# Descriptive title of project
+TITLE = "Cook County Convictions Data Project"
+
+# Google spreadsheet key
+SPREADSHEET_KEY = "11q1h1ft59mVZbUWMqIiWN4nUh1tQA7rZYxMaU0NcXS0"
+
+# Exclude these files from publication
+EXCLUDES = ["*.md", "requirements.txt"]
+
+# Create JSON data at ./data.json, disabled by default
+# CREATE_JSON = True
+
+# S3 bucket configuration
+S3_BUCKETS = {
+    # Provide target -> s3 url pairs, such as:
+    #     "mytarget": "mys3url.bucket.url/some/path"
+    # then use tarbell publish mytarget to publish to it
+    "production": "<WHATEVER>/cook-convictions",
+}
+
+# Default template variables
+DEFAULT_CONTEXT = {
+    'name': 'cook-convictions',
+    'title': 'Cook County Convictions Data Project'
+}
